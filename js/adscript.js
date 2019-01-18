220691a5371ffc50005360345039bd7e523a09f4
THEGUY <38544468+THEGUY3ds@users.noreply.github.com>
2019-01-18 14:19:44 +1100

    Add files via upload

---
+++ js/adscript.js
@@ -0,0 +1,10 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6199326973101336"
+     data-ad-slot="9926261603"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
