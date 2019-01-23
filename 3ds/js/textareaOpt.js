0e078ae2711f4a729a2a83dc98e47a0883501cce
THEGUY <38544468+THEGUY3ds@users.noreply.github.com>
2019-01-23 12:49:40 +1100

    Add files via upload

---
+++ 3ds/js/textareaOpt.js
@@ -0,0 +1,11 @@
+var defaultReplyOpt = { //Quick reply options
+	lang: "en",
+	traceTextarea: true,
+	buttons: "bold,italic,underline,strike,|,img,link,|,fontcolor,|,removeFormat"
+}
+
+var defaultNewCodesOpt = { //Submit a Code options
+	lang: "en",
+	traceTextarea: true,
+	buttons: "bold,italic,underline,strike,|,img,link,|,fontcolor,|,removeFormat"
+}
